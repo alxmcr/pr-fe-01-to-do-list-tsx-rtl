9d32ffe531941ce1781be3e8552c07d389532bd9
Alejandro Coca <alxmcr@outlook.com>
2025-08-09 16:06:48 -0400

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { routes } from "./router/routes.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+// Router
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
