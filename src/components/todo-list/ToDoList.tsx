2bdac3ec3f3fd0eaf50889a5ff985e92bb94cebe
Alejandro Coca <alxmcr@outlook.com>
2025-08-09 16:56:54 -0400

    wip

---
+++ src/components/todo-list/ToDoList.tsx
@@ -0,0 +1,19 @@
+import type { ToDoItemData } from "../../services/todo-list-service/IToDoListService";
+import { ToDoItem } from "../todo-item/ToDoItem";
+import "./ToDoList.styles.css";
+
+type Props = {
+  todos: ToDoItemData[];
+};
+
+export function ToDoList(props: Props) {
+  if (props.todos?.length === 0) return <p>Todo list is empty</p>;
+
+  return (
+    <>
+      {props.todos.map((todo: ToDoItemData) => (
+        <ToDoItem key={todo.id} todo={todo} />
+      ))}
+    </>
+  );
+}
