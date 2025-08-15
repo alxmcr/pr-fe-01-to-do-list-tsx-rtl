6cbf8855deccc0948bd3d734435302b503ea911c
Alejandro Coca <alxmcr@outlook.com>
2025-08-15 00:57:19 -0400

    wip

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default defineConfig({
   e2e: {
-    baseUrl: `http://localhost:${process.env.VITE_APP_PORT || 3000}`,
+    baseUrl: `http://localhost:${
+      process.env.VITE_APP_PORT || process.env.CYPRESS_VITE_APP_PORT || 3000
+    }`,
     supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     viewportWidth: 1280,
