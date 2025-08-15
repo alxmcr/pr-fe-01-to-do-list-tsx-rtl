b8fbbba876d241350cee9529dbb3cadc73abbda6
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-08-15 01:00:22 -0400

    fix/cypress-setup-001 (#9)

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default defineConfig({
   e2e: {
-    baseUrl: `http://localhost:${process.env.VITE_APP_PORT || 7100}`,
+    baseUrl: `http://localhost:${
+      process.env.VITE_APP_PORT || process.env.CYPRESS_VITE_APP_PORT || 3000
+    }`,
     supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     viewportWidth: 1280,
@@ -11,6 +17,8 @@ export default defineConfig({
     screenshotOnRunFailure: false,
     // Add options to reduce warnings
     experimentalModifyObstructiveThirdPartyCode: false,
+    // Improve performance
+    numTestsKeptInMemory: 0,
   },
   component: {
     devServer: {
