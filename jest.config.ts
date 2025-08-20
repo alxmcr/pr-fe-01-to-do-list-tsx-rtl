a2ab4bc6ac28508b85a5923cf42462158026ff00
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-08-19 21:52:36 -0400

    chore/write-comment-pr-with-gh-actions (#26)

--- jest.config.ts
+++ jest.config.ts
@@ -112,7 +112,7 @@ const config: Config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: ["default", ["jest-junit", { suiteName: "jest tests" }]],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
